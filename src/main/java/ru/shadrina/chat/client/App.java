497e75a8929e9dd75f2e1745481f107e7e25c1a8
oashvgr <oashadrina92@gmail.com>
2018-09-27 15:41:50 +0300

    Initial commit

---
+++ src/main/java/ru/shadrina/chat/client/App.java
@@ -0,0 +1,13 @@
+package ru.shadrina.chat.client;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
